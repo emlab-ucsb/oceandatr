0e6802495c052d7832d1e9e9988dacac3be81e93
Jason Flower <jflowernet@aim.com>
2023-12-06 15:31:14 +1000

    help files update

--- R/get_planning_grid.R
+++ R/get_planning_grid.R
@@ -1,25 +1,25 @@
 #' Create a planning grid raster for an area of interest
 #' 
-#' @description This function creates a planning grid raster around the area of interest; this is used in several other functions to standardize the extent and CRS of downloaded data.
+#' @description This function creates a planning grid for area of interest.
 #'
 #' @param area_polygon an sf polygon or multipolygon object of the area of interest (e.g., a country's EEZ)
-#' @param projection_crs a suitable crs for the area of interest (must be in meters); for prioritization work, best practices is to use a local, equal area projection in WKT format
+#' @param projection_crs a suitable crs for the area of interest; for prioritization work, best practices is to use a local, equal area projection
 #' @param option the desired output format, either "raster", "sf_square" (vector), or "sf_hex" (vector); default is "raster"
-#' @param resolution_km numeric; the desired planning unit (grid cell) resolution in km
+#' @param resolution numeric; the desired planning unit (grid cell) resolution in units (usually metres or degrees) of the projection_crs: `sf::st_crs(projection_crs, parameters = TRUE)$units_gdal`
 #'
-#' @return A raster planning grid of the same resolution and crs provided
+#' @return A `terra::rast()` of `sf` planning grid of the same resolution and crs provided
 #' @export
 #'
 #' @examples 
-#' # Grab EEZ data first 
+#' # Get ar area of interest first. In this case Bermuda's EEZ
 #' bermuda_eez <- get_area(area_name = "Bermuda")
-#' # Create a CRS using a local, equal area projection in WKT format for Bermuda. 
-#' # You can get WKT outputs in equal area projections for your area of interest from https://projectionwizard.org
-#' projection <- 'PROJCS["ProjWiz_Custom_Lambert_Azimuthal", GEOGCS["GCS_WGS_1984", DATUM["D_WGS_1984", SPHEROID["WGS_1984",6378137.0,298.257223563]], PRIMEM["Greenwich",0.0], UNIT["Degree",0.0174532925199433]], PROJECTION["Lambert_Azimuthal_Equal_Area"], PARAMETER["False_Easting",0.0], PARAMETER["False_Northing",0.0], PARAMETER["Central_Meridian",-64.5], PARAMETER["Latitude_Of_Origin",32], UNIT["Meter",1.0]]'
-#' # Create a planning grid with 5 km resolution; reproject the `bermuda_eez` object to the CRS defined in the `projection` object
-#' planning_grid <- get_planning_grid(area_polygon = bermuda_eez, projection_crs = projection, resolution_km = 5)
+#' # Create a CRS using a local, equal area projection for Bermuda. 
+#' # You can a suitable equal area projection for your area of interest from https://projectionwizard.org
+#' projection <- '+proj=laea +lon_0=-64.8108333 +lat_0=32.3571917 +datum=WGS84 +units=m +no_defs'
+#' # Create a planning grid with 5 km (5000 m) resolution covering the `bermuda_eez` in projection specified by `projection_crs` object
+#' planning_grid <- get_planning_grid(area_polygon = bermuda_eez, projection_crs = projection, resolution = 5000)
 
-get_planning_grid <- function(area_polygon, projection_crs, option = "raster", resolution_km = 5){
+get_planning_grid <- function(area_polygon, projection_crs, option = "raster", resolution = 5000){
   
   # Add repeated errors for area_polygon
   if(!check_sf(area_polygon)) { 
@@ -34,11 +34,11 @@ get_planning_grid <- function(area_polygon, projection_crs, option = "raster", r
   
   if(option == "raster") { 
     area_polygon %>% 
-      terra::rast(resolution = resolution_km*1000) %>% 
+      terra::rast(resolution = resolution) %>% 
       terra::rasterize(area_polygon, ., touches=FALSE, field = 1)
     
   } else{
-    grid_out <- if(option == "sf_square") sf::st_make_grid(area_polygon, cellsize = resolution_km*1000, square = TRUE) %>% sf::st_as_sf() else sf::st_make_grid(area_polygon, cellsize = resolution_km*1000, square = FALSE) %>% sf::st_as_sf() 
+    grid_out <- if(option == "sf_square") sf::st_make_grid(area_polygon, cellsize = resolution_km*1000, square = TRUE) %>% sf::st_as_sf() else sf::st_make_grid(area_polygon, cellsize = resolution, square = FALSE) %>% sf::st_as_sf() 
   
     grid_centroids <- sf::st_centroid(grid_out)
     
