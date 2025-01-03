8c2dcd2abcd2f946a83a7294491386384675c8a9
Jason Flower <jflowernet@aim.com>
2025-01-03 13:31:09 +1000

    test updates, add test helper functions

--- tests/testthat/test-get_dist.R
+++ tests/testthat/test-get_dist.R
@@ -14,11 +14,11 @@ test_that("returns gridded distance to shore for Kiribati - sf", {
 })
 
 test_that("returns gridded distance to port for Kiribati - raster", {
-  expect_s4_class(suppressWarnings(get_dist(spatial_grid = get_grid(boundary = get_boundary(name = "Kiribati", country_type = "sovereign"), 
+  expect_s3_class(suppressWarnings(get_dist(spatial_grid = get_grid(boundary = get_boundary(name = "Kiribati", country_type = "sovereign"), 
                                                                     crs = '+proj=laea +lon_0=-159.609375 +lat_0=0 +datum=WGS84 +units=m +no_defs', 
                                                                     resolution = 50000, 
                                                                     output = "sf_hex"), dist_to = "ports")), 
-                  class = "SpatRaster")
+                  class = "sf")
 })
 
 test_that("returns port point data for Bermuda", {
