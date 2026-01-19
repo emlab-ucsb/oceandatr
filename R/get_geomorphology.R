0fdf5d6e171532fd376855e2e76ed57ef9d72dfe
Jason Flower <jflowernet@aim.com>
2026-01-19 15:56:03 +1000

    tidy up vignette and help

--- R/get_geomorphology.R
+++ R/get_geomorphology.R
@@ -60,10 +60,10 @@
 #' bermuda_eez <- get_boundary(name = "Bermuda")
 #' # Get geomorphology for the EEZ
 #' bermuda_geomorph <- get_geomorphology(spatial_grid = bermuda_eez, raw = TRUE)
-#' plot(bermuda_geomorph)
 #' # Get geomorphological features in spatial_grid
 #' bermuda_grid <- get_grid(boundary = bermuda_eez, crs = '+proj=laea +lon_0=-64.8108333 +lat_0=32.3571917 +datum=WGS84 +units=m +no_defs', resolution = 20000)
-#' geomorph_gridded <- get_geomorphology(spatial_grid = bermuda_grid)
+#' geomorph_gridded <- get_geomorphology(spatial_grid = bermuda_grid) %>% remove_empty_layers() #helper function to remove data layers that are all zero or NA values
+#' plot(geomorph_gridded)
 get_geomorphology <- function(spatial_grid = NULL, raw = FALSE, antimeridian = NULL){
   
   check_grid(spatial_grid)
